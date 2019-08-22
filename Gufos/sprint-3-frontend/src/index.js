b01e2b813861a856fd3aeb69072415e6f333708d
Saulo <saulo.macedoprojetos@gmail.com>
2019-08-22 19:52:06 -0300

    Adiciona projeto Gufos

---
+++ Gufos/sprint-3-frontend/src/index.js
@@ -0,0 +1,60 @@
+import React from "react";
+import ReactDOM from "react-dom";
+// navegação
+import {
+  Route,
+  Link,
+  BrowserRouter as Router,
+  Switch,
+  Redirect
+} from "react-router-dom";
+
+import "./index.css";
+import App from "./pages/Home/App";
+// incluir página
+import Categorias from "./pages/Categorias/Categorias";
+import Eventos from "./pages/Eventos/Eventos";
+
+import Login from "./pages/Login/Login";
+
+import NaoEncontrada from "./pages/Default/NaoEncontrada";
+
+import * as serviceWorker from "./serviceWorker";
+
+const RotaPrivada = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={props =>
+      localStorage.getItem("usuario-gufos") !== null ? (
+        <Component {...props} />
+      ) : (
+        <Redirect
+          to={{ pathname: "/login", state: { from: props.location } }}
+        />
+      )
+    }
+  />
+);
+
+const routing = (
+  <Router>
+    <div>
+      <Switch>
+        <Route exact path="/" component={App} />
+        {/* <Route path="/categorias" component={Categorias} /> */}
+        <RotaPrivada path="/categorias" component={Categorias} />
+        <RotaPrivada path="/eventos" component={Eventos} />
+        <Route path="/login" component={Login} />
+        <Route component={NaoEncontrada} />
+      </Switch>
+    </div>
+  </Router>
+);
+
+// ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(routing, document.getElementById("root"));
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
---
+++ Gufos/sprint-3-frontend/src/pages/Categorias/Categorias.js
@@ -0,0 +1,135 @@
+import React, { Component } from "react";
+
+// realizar os imports dos estilos
+import "../../assets/css/flexbox.css";
+import "../../assets/css/reset.css";
+import "../../assets/css/style.css";
+
+// import do ícone
+import logo from "../../assets/img/icon-login.png";
+
+// importar Rodape
+import Rodape from "../../componentes/Rodape";
+
+import Titulo from "../../componentes/Titulo";
+
+class Categorias extends Component {
+  constructor() {
+    super();
+    this.state = {
+      lista: [],
+      nome: ""
+      //   [
+      //     { idCategoria: 1, nome: "Design" },
+      //     { idCategoria: 2, nome: "Jogos" },
+      //     { idCategoria: 3, nome: "Meetup" }
+      //   ]
+    };
+
+    this.cadastrarCategoria = this.cadastrarCategoria.bind(this);
+  }
+
+  cadastrarCategoria(event) {
+    event.preventDefault();
+    fetch("http://localhost:5000/api/categorias", {
+      method: "POST",
+      body: JSON.stringify({ nome: this.state.nome }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(response => response.json())
+      .then(this.listarCategorias())
+      .catch(error => console.log(error));
+  }
+
+  atualizaNome(event) {
+    this.setState({ nome: event.target.value });
+  }
+
+  listarCategorias() {
+    fetch("http://localhost:5000/api/categorias")
+      .then(response => response.json())
+      .then(data => this.setState({ lista: data }));
+  }
+
+  componentDidMount() {
+    this.listarCategorias();
+  }
+
+  render() {
+    return (
+      <div>
+        <header className="cabecalhoPrincipal">
+          <div className="container">
+            <img src={logo} />
+          </div>
+        </header>
+
+        <main className="conteudoPrincipal">
+          <section className="conteudoPrincipal-cadastro">
+            {/* <h1 className="conteudoPrincipal-cadastro-titulo">Categorias</h1> */}
+            <Titulo titulo="Categorias" />
+            <div className="container" id="conteudoPrincipal-lista">
+              <table id="tabela-lista">
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>Título</th>
+                  </tr>
+                </thead>
+
+                <tbody id="tabela-lista-corpo">
+                  {this.state.lista.map(function(categoria) {
+                    return (
+                      <tr key={categoria.idCategoria}>
+                        <td>{categoria.idCategoria}</td>
+                        <td>{categoria.nome}</td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+            </div>
+
+            <div className="container" id="conteudoPrincipal-cadastro">
+              <h2 className="conteudoPrincipal-cadastro-titulo">
+                Cadastrar Categoria
+              </h2>
+              <form onSubmit={this.cadastrarCategoria}>
+                <div className="container">
+                  <input
+                    type="text"
+                    value={this.state.nome}
+                    onChange={this.atualizaNome.bind(this)}
+                    className="className__categoria"
+                    id="input__categoria"
+                    placeholder="tipo do evento"
+                  />
+                  <button
+                    id="btn__cadastrar"
+                    className="conteudoPrincipal-btn conteudoPrincipal-btn-cadastro"
+                  >
+                    Cadastrar
+                  </button>
+                </div>
+              </form>
+            </div>
+          </section>
+        </main>
+
+        <Rodape />
+
+        {/* <footer className="rodapePrincipal">
+          <section className="rodapePrincipal-patrocinadores">
+            <div className="container">
+              <p>Escola SENAI de Informática - 2019</p>
+            </div>
+          </section>
+        </footer> */}
+      </div>
+    );
+  }
+}
+
+export default Categorias;
